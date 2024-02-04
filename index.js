19b7ab2ad97d1c97bbc7d9db05adf48c13083ca1
pwangy <blipsandclicks@gmail.com>
2024-02-03 16:42:46 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = drivers => {
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = drivers => {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (fareMultiplier) => {
+    return (value) => {
+        return fareMultiplier * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const fareQuintupler = createFareMultiplier(5)
+
+const selectDifferentDrivers = (drivers, selectingDrivers) => {
+    return selectingDrivers(drivers)
+}
